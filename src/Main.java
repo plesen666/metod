11054c258d0c29c28157833c2d2acbdf4a7b6611
plesen666 <diada.var@mail.ru>
2024-08-01 10:44:54 +0300

    hw4

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,46 @@
-import java.time.LocalDate;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static boolean checkLeapYear(int year) {
+        return (year > 1524 && (year % 4) == 0 && year % 100 != 0 || (year % 400 == 0));
+
+    }
+    public static String installSistem(int os, int yearProd) {
+        int currentYear = 2015;
+        String msg = " Установите %s версию приложения для %s  по ссылке ";
+        String osName = os == 0 ? "iOs" : "Android";
+        String version = yearProd >= currentYear ? "" : "облегченую";
+        return String.format(msg, version, osName);
+    }
 
 
-        public static void isYear (int year) {
-            if (year > 1524 && (year % 4) == 0 && year % 100 != 0 || (year % 400 == 0)){
-                System.out.println(year + " год является високосным");
-            } else {
-                System.out.println(year + " год не является високосным");
+    public static void countDay(int deliveriDistance) {
+        if (deliveriDistance <= 100) {
+            if (deliveriDistance <= 20) {
+                System.out.println(" Доставка в пределах" + deliveriDistance + " км. занимает сутки.");
+            } else if (deliveriDistance > 20 && deliveriDistance < 60) {
+                System.out.println(" Доставка в пределах" + deliveriDistance + " км. занимает двое суток.");
+            } else if (deliveriDistance >= 60 && deliveriDistance < 100) {
+                System.out.println(" Доставка в пределах" + deliveriDistance + " км. занимает трое суток.");
             }
+        } else {
+            System.out.println(" На такое расстояние не доставляем.");
         }
-    public static void instalSistem (int os, int yearProd) {
-        int currentYear = LocalDate.now().getYear();
-        if (os == 0 && yearProd == currentYear) {
-            System.out.println("Установите версию приложения для Ios по ссылке");
-        } else if (os == 0 && yearProd < currentYear) {
-            System.out.println("Установите облегченную версию приложения для Ios по ссылке");
-        } else if (os == 1 && yearProd == currentYear) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (os == 1 && yearProd < currentYear) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-           }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Задание 1.");
+        int year = 2025;
+        checkLeapYear(year);
+        if (checkLeapYear(year)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
         }
+        System.out.println("Задание 2.");
+        System.out.println(installSistem(0, 2015));
 
-        public static void countDay( int deliveriDistance){
-            if (deliveriDistance<=100){
-                if(deliveriDistance<20){
-                    System.out.println( " Доставка в пределах" + deliveriDistance + " км. занимает сутки.");
-                } else if ( deliveriDistance>=20 && deliveriDistance<60){
-                    System.out.println( " Доставка в пределах" + deliveriDistance + " км. занимает двое суток." );
-                } else if ( deliveriDistance >60&&deliveriDistance<=100) {
-                    System.out.println( " Доставка в пределах" + deliveriDistance + " км. занимает трое суток." );
-                }
-            }else {
-        System.out.println( " На такое расстояние не доставляем.");
-                }
-}
-
-public static void main( String[]args){
-    System.out.println( " Задание 1.");
-    int year = 2025;
-    isYear(year);
-    System.out.println( " Задание 2.");
-    instalSistem( 0 ,2015);
-    System.out.println( " Задание 3.");
-    countDay(101);
+        System.out.println(" Задание 3.");
+        countDay(101);
 
 
-}
+    }
 }
